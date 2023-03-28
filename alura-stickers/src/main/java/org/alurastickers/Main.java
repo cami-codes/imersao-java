fab396bc0ed58e8dd43b507c806628fd6c0db63a
Camila <camila.matosrocha1234@gmail.com>
2023-03-28 10:13:58 -0300

    Desafios concluídos

--- alura-stickers/src/main/java/org/alurastickers/Main.java
+++ alura-stickers/src/main/java/org/alurastickers/Main.java
@@ -7,30 +7,58 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        // 1. Fazer uma conexão HTTP e buscar os top 250 filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
+        // 1. Get API key from config file
+        String key = ResourceBundle.getBundle("config").getString("api.key");
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        // 2. Build URL and send HTTP request to get top 250 movies from IMDB API
+        String urlTop250Movies = "https://imdb-api.com/en/API/Top250Movies/" + key;
+        URI addressMovies = URI.create(urlTop250Movies);
+
+        var client = HttpClient.newHttpClient();
+        var requestMovies = HttpRequest.newBuilder(addressMovies).GET().build();
+        HttpResponse<String> response = client.send(requestMovies, HttpResponse.BodyHandlers.ofString());
         String body = response.body();
 
-        System.out.println(body);
-
-        // 2. Extrair só os dados que interessam (título, pôster e classificação)
+        // 3. Parse JSON response to extract relevant data (title, poster, and rating)
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        List<Map<String, String>> moviesList = parser.parse(body);
 
-        // 3. Exibir e manipular os dados
-        for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+        // 4. Display and manipulate the data
+        for (Map<String, String> movie : moviesList) {
+            String title = movie.get("title");
+            String image = movie.get("image");
+            String ratingString = movie.get("imDbRating");
+
+            // Add movie and rating emojis to title string
+            title += " 🎬";
+            ratingString += " ⭐";
+
+            // Parse rating string to a double value and calculate number of stars
+            double ratingParse = Double.parseDouble(ratingString.replaceAll("[^0-9.]", ""));
+            int starCount = 10;
+
+            // Build star string based on number of stars and rating
+            String stars = "";
+            for (int i = 1; i <= starCount; i++) {
+                if (i <= ratingParse) {
+                    stars += "★";
+                } else {
+                    stars += "☆";
+                }
+            }
+
+            // Build rating string with star string and original rating string
+            ratingString = "Classificação: " + stars + " (" + ratingString + ")";
+
+            // Print movie title, poster image, and rating
+            System.out.println("\033[1m" + title + "\033[0m");
+            System.out.println(image);
+            System.out.println("\033[33m" + ratingString + "\033[0m");
             System.out.println();
         }
     }
