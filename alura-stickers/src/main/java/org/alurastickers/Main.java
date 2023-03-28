b34b24a6f600433cb015699d137e0042777dfec3
Camila <camila.matosrocha1234@gmail.com>
2023-03-27 22:27:10 -0300

    Imersão Java Alura - Aula 1

---
+++ alura-stickers/src/main/java/org/alurastickers/Main.java
@@ -0,0 +1,37 @@
+package org.alurastickers;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        // 1. Fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+
+        System.out.println(body);
+
+        // 2. Extrair só os dados que interessam (título, pôster e classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // 3. Exibir e manipular os dados
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
